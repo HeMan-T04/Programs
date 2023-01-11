0bb8a1c304739bc14cb5ea9467db789b7221bbe9
hemant kathuria <100681483+HeMan-T04@users.noreply.github.com>
2023-01-11 16:52:07 +0000

    till Q35

--- C++/Q35.cpp
+++ C++/Q35.cpp
@@ -1,4 +1,4 @@
-//Write a Program to illustrate concept of constructors in derived class in which no constructor is used in base class and derived class
+//Write a Program to illustrate concept of constructors in derived class in which constructor is used only in derived class
 #include <iostream>
 using namespace std;
 class employee
@@ -24,11 +24,10 @@ class salary: public employee
 {
     int s;
     public:
-        void getdata()
+        salary(int a)
         {
             employee::getdata();
-            cout << "Enter salary: ";
-            cin >> a;
+            this->s = a;
         }
         void display()
         {
@@ -36,8 +35,11 @@ class salary: public employee
             cout << "Salary: " << s << endl;
         }
 };
-int main(){
-    salary ob;
-    ob.getdata();
+int main()
+{
+    int a;
+    cout << "Enter salary: ";
+    cin >> a;
+    salary ob(a);
     ob.display();
 }
