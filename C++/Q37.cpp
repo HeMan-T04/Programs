76ce570f3fe9b3550b1b279860adadefad8ffcb2
hemant kathuria <100681483+HeMan-T04@users.noreply.github.com>
2023-01-11 17:05:14 +0000

    till Q37

---
+++ C++/Q37.cpp
@@ -0,0 +1,47 @@
+//Write a Program to illustrate concept of constructors in derived class in which constructor is used in base class and derived class without default constructor
+#include <iostream>
+#include <cstring>
+using namespace std;
+class employee
+{
+    int emp_id;
+    char emp_name[20];
+    public:
+        employee(int a, char b[])
+        {
+            this->emp_id = a;
+            strcpy(this->emp_name, b);
+        }
+        void display()
+        {
+            cout << "Employee id: " << emp_id << endl;
+            cout << "Employee name: " << emp_name << endl;
+        }
+};
+class salary: public employee
+{
+    int s;
+    public:
+        salary(int a, char b[], int c): employee(a, b)
+        {
+            this->s = c;
+        }
+        void display()
+        {
+            employee::display();
+            cout << "Salary: " << s << endl;
+        }
+};
+int main()
+{
+    int a,c;
+    char b[20];
+    cout << "Enter employee id: ";
+    cin >> a;
+    cout << "Enter employee name: ";
+    cin >> b;
+    cout << "Enter salary: ";
+    cin >> c;
+    salary ob(a,b,c);
+    ob.display();
+}
