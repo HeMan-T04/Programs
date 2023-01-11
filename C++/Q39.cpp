c99a61c9a84a1c7161472e16ca2274fa7c2e95d5
hemant kathuria <100681483+HeMan-T04@users.noreply.github.com>
2023-01-11 17:48:47 +0000

    Q39 fixed

--- C++/Q39.cpp
+++ C++/Q39.cpp
@@ -1,47 +1,29 @@
-//Write a Program to illustrate concept of constructors in derived class in which Explicit invocation is there in absence of default constructor
-#include <iostream>
-#include <cstring>
+// Write a Program to illustrate concept of constructors in derived class in which Explicit invocation is there in absence of default constructor
+
 using namespace std;
-class employee
+
+#include <iostream>
+
+class Base
 {
-    int emp_id;
-    char emp_name[20];
-    public:
-        explicit employee(int a, char b[])
-        {
-            this->emp_id = a;
-            strcpy(this->emp_name, b);
-        }
-        void display()
-        {
-            cout << "Employee id: " << emp_id << endl;
-            cout << "Employee name: " << emp_name << endl;
-        }
+public:
+    Base(int x)
+    {
+        cout << "Base class constructor called with x = " << x << endl;
+    }
 };
-class salary: public employee
+
+class Derived : public Base
 {
-    int s;
-    public:
-        salary(int a, char b[], int c): employee(a, b)
-        {
-            this->s = c;
-        }
-        void display()
-        {
-            employee::display();
-            cout << "Salary: " << s << endl;
-        }
+public:
+    Derived(int x, int y) : Base(x)
+    {
+        cout << "Derived class constructor called with y = " << y << endl;
+    }
 };
+
 int main()
 {
-    int a,c;
-    char b[20];
-    cout << "Enter employee id: ";
-    cin >> a;
-    cout << "Enter employee name: ";
-    cin >> b;
-    cout << "Enter salary: ";
-    cin >> c;
-    salary ob(a,b,c);
-    ob.display();
+    Derived obj(10, 20);
+    return 0;
 }
