352f571f8fd41ed66a0e9678aa099a48edf0c0b8
hemant kathuria <100681483+HeMan-T04@users.noreply.github.com>
2023-01-11 15:43:23 +0000

    Q30 fixed

--- C++/Q30.cpp
+++ C++/Q30.cpp
@@ -1,65 +1,66 @@
-//Write a C++ program handling following details of a student and staff using inheritance 
+// Write a C++ program handling following details of a student and staff using inheritance
+//  Student details:  Name, Address, Percentage, Marks
+//  Staff details:  Name, Address, Salary
+//  Create appropriate base and derived classes. Input the details and output them
 #include <iostream>
 using namespace std;
 class person
 {
     char name[20];
-    int age;
-    public:
-        void getdata()
-        {
-            cout << "Enter name: ";
-            cin >> name;
-            cout << "Enter age: ";
-            cin >> age;
-        }
-        void display()
-        {
-            cout << "Name: " << name << endl;
-            cout << "Age: " << age << endl;
-        }
+    char address[30];
+
+public:
+    void getdata()
+    {
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter address: ";
+        cin >> address;
+    }
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Address: " << address << endl;
+    }
 };
-class student:public person
+class student : public person
 {
-    int rollno;
-    char course[20];
-    public:
-        void getdata()
-        {
-            person::getdata();
-            cout << "Enter roll number: ";
-            cin >> rollno;
-            cout << "Enter course: ";
-            cin >> course;
-        }
-        void display()
-        {
-            person::display();
-            cout << "Roll number: " << rollno << endl;
-            cout << "Course: " << course << endl;
-        }
+    float percentage;
+    int marks;
+public:
+    void getdata()
+    {
+        person::getdata();
+        cout << "Enter marks(Out of 500): ";
+        cin >> marks;
+        cout << "Enter Percentage: ";
+        cin >> percentage;
+    }
+    void display()
+    {
+        person::display();
+        cout << "Marks: " << marks << endl;
+        cout << "Percentage: " << percentage << endl;
+    }
 };
-class staff:public person
+class staff : public person
 {
-    int staffid;
-    char dept[20];
-    public:
-        void getdata()
-        {
-            person::getdata();
-            cout << "Enter staff id: ";
-            cin >> staffid;
-            cout << "Enter department: ";
-            cin >> dept;
-        }
-        void display()
-        {
-            person::display();
-            cout << "Staff id: " << staffid << endl;
-            cout << "Department: " << dept << endl;
-        }
+    int salary;
+public:
+    void getdata()
+    {
+        person::getdata();
+        cout << "Enter salary: ";
+        cin >> salary;
+    }
+    void display()
+    {
+        person::display();
+        cout << "Salary: " << salary << endl;
+    }
 };
-int main(){
+int main()
+{
     student s;
     staff t;
     cout << "Enter details of student" << endl;
