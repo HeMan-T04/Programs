33fd0170dc6fb62c00242da40d0559362ddeb582
hemant kathuria <100681483+HeMan-T04@users.noreply.github.com>
2023-01-11 18:15:21 +0000

    till Q43

---
+++ C++/Q43.cpp
@@ -0,0 +1,25 @@
+// Write a Program to illustrate concept of constructors in derived class in which constructor is used in multilevel inheritance
+#include <iostream>
+using namespace std;
+class Base1 {
+  public:
+    Base1() {
+        cout << "Base1 class default constructor called" << endl;
+    }
+};
+class derived1: public Base1 {
+  public:
+    derived1() {
+        cout << "derived1 class default constructor called" << endl;
+    }
+};
+class derived2: public derived1{
+    public:
+        derived2() {
+            cout << "derived2 class default constructor called" << endl;
+        }
+};
+int main() {
+    derived2 obj;
+    return 0;
+}
